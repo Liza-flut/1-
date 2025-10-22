50eb66d199569dbc4ee8e0b1ca218e269b203965
Liza <yelizavetasorokina70@gmail.com>
2025-10-22 23:27:08 +0500

    basket

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import "./card.css";
 import { BsFillBasket2Fill } from 'react-icons/bs';
 
-function Card ({ title,image, description}) {
+function Card ({ title,image, description, addToCart, resetKey}) {
+  const [isBuy, setIsBuy]=useState(false);
+  const handleBuy = () => {
+    setIsBuy(true);
+    addToCart();
+  };
+  useEffect(() => {
+    setIsBuy(false);
+  }, [resetKey]);
   return (
     <div className="card">
       <img src={image} alt={title} className="card-image" />
       <div className="card-content">
         <h2 className="card-title">{title}</h2>
         <p className="card-description">{description}</p>
-        <button className="card-button">
-          В корзину
+        <button onClick={handleBuy} className="card-button">
+          {!isBuy ? "Купить": "Куплено"}
           <BsFillBasket2Fill style={{ marginLeft: "8px" }} />
         </button>
       </div>
--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,10 +1,21 @@
 import "./Main.scss";
 import Card from "../components/Card";
 import React, { useState, useEffect } from "react";
+import { FaShoppingCart } from 'react-icons/fa';
 
 const MainPage = () => {
   const [cards, setCards] = useState([])
   const [isLoad, setIsLoad] = useState(true);
+  const [cartCount, setCartCount]= useState(0);
+  const [isPlus, setIsPlus]=useState(false);
+
+  const addToCart = () => {setCartCount(prev => prev + 1); setIsPlus(true)};
+  const clearCart = () => {
+    setCartCount(0);
+    setIsPlus(false);
+    setResetCards(prev => prev + 1);
+  };
+  const [resetCards, setResetCards] = useState(0);
 
   useEffect(() => {
     setIsLoad(true);
@@ -27,24 +38,68 @@ const MainPage = () => {
           image: "images/card.png",
           title: "112 000 ₽",
           description: "Кроссовки Nike Pegasus Limited Edition"
+        },
+        {
+          id: 4,
+          image: "images/card.png",
+          title: "112 000 ₽",
+          description: "Кроссовки Nike Pegasus Limited Edition"
         }
       ]);
       setIsLoad(false);
     }, 3000);
   },[])
   return(
-    <>
-    {isLoad == true ?  (
-        <div style={{display: "flex", alignItems: "center", justifyContent: "center", height: "10vh"}}>Загрузка</div>
+  <>
+    <div 
+        onClick={clearCart}
+        style={{
+        position: 'fixed',
+        top: '20px',
+        right: '15px',
+        background: 'black',
+        color: 'white',
+        padding: '10px 15px',
+        borderRadius: '8px',
+        fontSize: '14px',
+        zIndex: 1000
+      }}>
+        <FaShoppingCart style={{transform: 'scaleX(-1)',  marginRight: "8px"}}/>Корзина({!isPlus ? "": "+"}{cartCount})
+      </div>
+    <div style={{width: "100%", display: "flex", justifyContent: "center"}}>
+    <div className="col col-6">
+      {isLoad == true ?  (
+        <div style={{
+          height: "100vh",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center", 
+          alignItems: "center", 
+          flexDirection: "column" 
+        }}>
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
       ) : (
-        <div style={{display: "flex", gap:"20px", padding:"20px"}}>
+        <div style={{
+          display: "flex", 
+          flexWrap: "wrap", 
+          gap: "20px", 
+          padding: "20px", 
+          justifyContent: "center",
+          width: "100%",
+          marginTop: "50px"
+        }}>
           {cards.map((card) =>(
-            <Card key={card.id} title={card.title} image={card.image} description={card.description} /> 
+            <Card key={card.id} title={card.title} image={card.image} description={card.description} addToCart={addToCart} resetKey={resetCards}/> 
           ))}
         </div>
       )}
-    </>
-  )
+    </div>
+  </div>
+  </>
+)
 };
 
 export default MainPage;
